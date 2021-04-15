281c516df9bb698526901afb5d06f815005050d8
Сергей Шурин <s.shurin94@gmail.com>
2021-04-15 17:49:34 +0300

    store password at env instead full path

--- db/mongo.js
+++ db/mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const dbURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/test'
+const dbURL = process.env.MONGODB_URI ? `mongodb+srv://mskiles:${process.env.MONGODB_URI}@cluster0.4gi9n.mongodb.net/myFirstDatabase?retryWrites=true` : 'mongodb://localhost:27017/test'
 
 mongoose.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true})
 
